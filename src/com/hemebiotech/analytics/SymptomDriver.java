e3c125b1ea0cde28cb8552e3209045a81e1415d4
Quentin Schnurr <quentin.schnurr@gmail.com>
2021-08-05 11:25:00 +0200

    Changement de nom du fichier de Alex et nettoyage de la partie writer

--- src/com/hemebiotech/analytics/SymptomDriver.java
+++ src/com/hemebiotech/analytics/SymptomDriver.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
-public class AnalyticsCounter {
+public class SymptomDriver {
 	private static int headacheCount = 0;	// initialize to 0
 	private static int rashCount = 0;		
 	private static int pupilCount = 0;		
@@ -33,11 +33,6 @@ public class AnalyticsCounter {
 			line = reader.readLine();	// get another symptom
 		}
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		
 	}
 }
