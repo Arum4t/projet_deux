df94f73c9a4b14738b1c9f8a64a2ff4178df9281
Quentin Schnurr <quentin.schnurr@gmail.com>
2021-08-05 11:10:03 +0200

    Création d'une boucle foreach

--- src/com/hemebiotech/analytics/SymptomIteration.java
+++ src/com/hemebiotech/analytics/SymptomIteration.java
@@ -7,8 +7,16 @@ public class SymptomIteration implements ISymptomIteration {
 
 	@Override
 	public TreeMap<String, Integer> sortSymptom(List<String> result) {
-	
+		
+		TreeMap<String, Integer> symptomsMap = new TreeMap<>();
+		
+		for (String symptoms : result) {
+			//Test if symptoms exist in map
+			
+			
+		}
 		return null;
 	}
 
 }
+
