fcaf877c6db4c496426d2f9b7cf97171624faf70
Quentin Schnurr <quentin.schnurr@gmail.com>
2021-08-05 11:03:37 +0200

    Création d'un interface afin de trier et compter les occurences de la liste et les regrouper dans un TreeMap

---
+++ src/com/hemebiotech/analytics/ISymptomIteration.java
@@ -0,0 +1,9 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.TreeMap;
+
+public interface ISymptomIteration {
+
+	TreeMap<String, Integer> sortSymptom (List<String>result);
+}
