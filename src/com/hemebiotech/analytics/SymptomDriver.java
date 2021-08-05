a7021698c5d179df8cbf8774aac3399c46931136
Quentin Schnurr <quentin.schnurr@gmail.com>
2021-08-05 11:43:17 +0200

    Tri de la liste

--- src/com/hemebiotech/analytics/SymptomDriver.java
+++ src/com/hemebiotech/analytics/SymptomDriver.java
@@ -1,6 +1,7 @@
 package com.hemebiotech.analytics;
 
 import java.util.List;
+import java.util.TreeMap;
 
 
 
@@ -8,10 +9,17 @@ public class SymptomDriver {
 	
 	public static void main (String[] args) {
 		// get the list of symptoms
-				List<String> fruits;
+				List<String> symptoms;
 				
 				ISymptomReader iSymptomReader = new ReadSymptomDataFromFile("symptoms.txt");
 				
-				fruits = iSymptomReader.getSymptoms();
+				symptoms = iSymptomReader.getSymptoms();
+				
+				//Sorting the list
+				TreeMap<String, Integer> symptomsMap;
+				
+				ISymptomIteration iSymptomIteration = new SymptomIteration();
+				
+				symptomsMap = iSymptomIteration.sortSymptoms(symptoms);
 	}
 }
