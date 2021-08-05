b0e898c37ba3b43f1e79f93171606fe47453d266
Quentin Schnurr <quentin.schnurr@gmail.com>
2021-08-05 11:05:35 +0200

    Création d'une classe qui implémente l'interface de tri.

---
+++ src/com/hemebiotech/analytics/SymptomIteration.java
@@ -0,0 +1,14 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.TreeMap;
+
+public class SymptomIteration implements ISymptomIteration {
+
+	@Override
+	public TreeMap<String, Integer> sortSymptom(List<String> result) {
+	
+		return null;
+	}
+
+}
