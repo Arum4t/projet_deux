059a4889879ff6a429e5cd6a312e9ff5748f9868
Quentin Schnurr <quentin.schnurr@gmail.com>
2021-08-05 11:28:01 +0200

    Création du code d'écriture dans le fichier result.out

--- src/com/hemebiotech/analytics/SymptomWrite.java
+++ src/com/hemebiotech/analytics/SymptomWrite.java
@@ -1,12 +1,27 @@
 package com.hemebiotech.analytics;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class SymptomWrite implements ISymptomWrite {
 
 	@Override
 	public void writeSymptoms(TreeMap<String, Integer> symptomsMap) {
-		
+
+		try {
+			FileWriter writer = new FileWriter("result.out");
+			for (Map.Entry<String, Integer> entry : symptomsMap.entrySet()) {
+				String key = entry.getKey();
+				Integer element = entry.getValue();
+				System.out.println(key + " : " + element.intValue());
+				writer.write(key + " : " + element.intValue() + "\n");
+			}
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
