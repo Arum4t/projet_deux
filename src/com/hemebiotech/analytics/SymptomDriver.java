1b2fa156eb363e5ba76930bf09a8f80c4b9dd037
Quentin Schnurr <quentin.schnurr@gmail.com>
2021-08-05 11:46:37 +0200

    Indentation

--- src/com/hemebiotech/analytics/SymptomDriver.java
+++ src/com/hemebiotech/analytics/SymptomDriver.java
@@ -3,27 +3,26 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.TreeMap;
 
-
 public class SymptomDriver {
-	
-	public static void main (String[] args) {
+
+	public static void main(String[] args) {
 		// get the list of symptoms
-				List<String> symptoms;
-				
-				ISymptomReader iSymptomReader = new ReadSymptomDataFromFile("symptoms.txt");
-				
-				symptoms = iSymptomReader.getSymptoms();
-				
-				//Sorting the list
-				TreeMap<String, Integer> symptomsMap;
-				
-				ISymptomIteration iSymptomIteration = new SymptomIteration();
-				
-				symptomsMap = iSymptomIteration.sortSymptoms(symptoms);
-				
-				//Display the list and write the list in result.out
-				ISymptomWrite iSymptomWrite = new SymptomWrite();
-				
-				iSymptomWrite.writeSymptoms(symptomsMap);
+		List<String> symptoms;
+
+		ISymptomReader iSymptomReader = new ReadSymptomDataFromFile("symptoms.txt");
+
+		symptoms = iSymptomReader.getSymptoms();
+
+		// Sorting the list
+		TreeMap<String, Integer> symptomsMap;
+
+		ISymptomIteration iSymptomIteration = new SymptomIteration();
+
+		symptomsMap = iSymptomIteration.sortSymptoms(symptoms);
+
+		// Display the list and write the list in result.out
+		ISymptomWrite iSymptomWrite = new SymptomWrite();
+
+		iSymptomWrite.writeSymptoms(symptomsMap);
 	}
 }
