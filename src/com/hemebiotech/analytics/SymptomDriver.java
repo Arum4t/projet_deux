acaf955c44f89303e6375ef47913fad5b9493feb
Quentin Schnurr <quentin.schnurr@gmail.com>
2021-08-05 11:35:20 +0200

    Appel de la méthode getSymptoms de ISymptomReader

--- src/com/hemebiotech/analytics/SymptomDriver.java
+++ src/com/hemebiotech/analytics/SymptomDriver.java
@@ -1,8 +1,17 @@
 package com.hemebiotech.analytics;
 
+import java.util.List;
+
+
+
 public class SymptomDriver {
 	
 	public static void main (String[] args) {
-		
+		// get the list of symptoms
+				List<String> fruits;
+				
+				ISymptomReader iSymptomReader = new ReadSymptomDataFromFile("symptoms.txt");
+				
+				fruits = iSymptomReader.getSymptoms();
 	}
 }
