6852d6ecc7ef848cc7fb7e304e148443ed9b753c
Quentin Schnurr <quentin.schnurr@gmail.com>
2021-09-10 13:54:06 +0200

    Fermeture des ressources avec Try with ressources

--- src/com/hemebiotech/write/SymptomWrite.java
+++ src/com/hemebiotech/write/SymptomWrite.java
@@ -6,27 +6,27 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class SymptomWrite implements ISymptomWrite {
-	
+
 	/**
-	 * Read all key and value from TreeMap 
+	 * Read all key and value from TreeMap
+	 * 
 	 * @see SymptomsIteration
 	 * 
-	 * Get key, get value and write them in a file name result.out
+	 *      Get key, get value and write them in a file name result.out
 	 * 
 	 */
-	
+
 	@Override
 	public void writeSymptoms(TreeMap<String, Integer> symptomsMap) {
 
-		try {
-			FileWriter writer = new FileWriter("result.out");
+		try (FileWriter writer = new FileWriter("result.out")) {
+
 			for (Map.Entry<String, Integer> entry : symptomsMap.entrySet()) {
 				String key = entry.getKey();
 				Integer element = entry.getValue();
 				System.out.println(key + " : " + element.intValue());
 				writer.write(key + " : " + element.intValue() + "\n");
 			}
-			writer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
